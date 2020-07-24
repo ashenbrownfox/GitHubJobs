ccb62d58c57729eda6aa8e2b3c2078d487ca6620
Ailun Shen <newhoopsdynasty@gmail.com>
2020-07-24 14:57:33 -0400

    bypass the CORS issue with this service

--- jobs/src/UseFetchJobs.js
+++ jobs/src/UseFetchJobs.js
@@ -7,7 +7,7 @@ const ACTIONS = {
     ERROR: 'error',
     UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
-const BASE_URL = "https://jobs.github.com/positions.json"
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
 function reducer(state,action){
     switch (action.type) {
         case ACTIONS.MAKE_REQUEST:
